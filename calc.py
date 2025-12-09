fa049d05aa9ed0238b3097171c78997c76214692
fabio9923 <eragonvaldespaz@gmail.com>
2025-12-09 10:54:44 -0600

    Mensaje de bienvenida agregado

--- calc.py
+++ calc.py
@@ -1,5 +1,7 @@
 # Calculadora para proyecto Docker + GitHub CI/CD
 def calcular():
+    print("Bienvenido a mi proyecto de Docker!")
+
     print("=== Calculadora en Docker ===")
     operacion = input("Ingresa una operación (ej: 5+3): ")
 
