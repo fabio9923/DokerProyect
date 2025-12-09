675c706d89dba4fbcc5e0cb3d01d5f1b716e10fa
fabio9923 <eragonvaldespaz@gmail.com>
2025-12-09 10:56:17 -0600

    Validación de input vacío

--- calc.py
+++ calc.py
@@ -4,6 +4,9 @@ def calcular():
 
     print("=== Calculadora en Docker ===")
     operacion = input("Ingresa una operación (ej: 5+3): ")
+    if operacion.strip() == "":
+        print("No ingresaste nada")
+        return
 
     try:
         resultado = eval(operacion)
